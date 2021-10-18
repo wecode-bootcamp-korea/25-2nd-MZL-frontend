be977b0f4611dae2355b42c06105666516cc720b
윤수연 <sueyeon@yunsuyeons-MacBook-Pro.local>
2021-10-18 17:56:15 +0900

    Add: fontawesome

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,7 +5,7 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
  * {
    box-sizing: border-box;
-   font-family: -apple-system, BlinkMacSystemFont, "Malgun Gothic", "맑은 고딕", Roboto, Helvetica, Arial, sans-serif;
+  
  }
 	
 `;
