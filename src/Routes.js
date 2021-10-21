e0b56b766b2b3f358876e794286a37e8ed5edcb1
goun <sgw7546@gmail.com>
2021-10-21 10:09:03 +0900

    Add: 비행 스케줄 컴포넌트 레이아웃 추가 및 받아올 props 적용

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import Main from './pages/main/Main';
+import Flightschedule from './components/Flightschedule/Flightschedule';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Nav />
+
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route exact path="/fs" component={Flightschedule} />
         </Switch>
         <Footer />
       </Router>
