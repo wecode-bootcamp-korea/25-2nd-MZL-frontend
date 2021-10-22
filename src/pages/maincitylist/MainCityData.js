d04b606ac2351b6a1edc6b9138be9ea39ceab535
윤수연 <sueyeon@yunsuyeons-MacBook-Pro.local>
2021-10-22 21:49:15 +0900

    conflict 해결 Modify: 목데이터 상수데이터로 변경

---
+++ src/pages/maincitylist/MainCityData.js
@@ -0,0 +1,60 @@
+const MAINCITYDATA = [
+  {
+    id: 0,
+    title: '경기도',
+    img_url:
+      'https://images.unsplash.com/photo-1627262212983-93287f00efc8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=850&q=80',
+    product_count: '2,500',
+  },
+  {
+    id: 1,
+    title: '제주도',
+    img_url:
+      'https://images.unsplash.com/photo-1579169326371-ccb4e63f7889?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8amVqdXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    product_count: '3,300',
+  },
+  {
+    id: 2,
+    title: '서울',
+    img_url:
+      'https://images.unsplash.com/photo-1598932248380-2b1c90a6c858?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHNlb3VsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    product_count: '1,410',
+  },
+  {
+    id: 3,
+    title: '경주',
+    img_url:
+      'https://images.unsplash.com/photo-1607416180948-f92ae7d9524a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=844&q=80',
+    product_count: '110',
+  },
+  {
+    id: 4,
+    title: '하와이',
+    img_url:
+      'https://images.unsplash.com/photo-1573993228224-5ffd3f00511c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=974&q=80',
+    product_count: '220',
+  },
+  {
+    id: 5,
+    title: '뉴욕',
+    img_url:
+      'https://images.unsplash.com/photo-1579961861832-6fecf4b86d1f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    product_count: '330',
+  },
+  {
+    id: 6,
+    title: '파리',
+    img_url:
+      'https://images.unsplash.com/photo-1509439581779-6298f75bf6e5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGFyaXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    product_count: '790',
+  },
+  {
+    id: 7,
+    title: '프라하',
+    img_url:
+      'https://images.unsplash.com/photo-1581200309617-836d0d7f9c04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80',
+    product_count: '50',
+  },
+];
+
+export default MAINCITYDATA;
